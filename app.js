12f4bbff3ad2680dabe3db8ef346616b73b4905b
unknown <marisela.delgadillo.cbtis37@gmail.com>
2020-09-11 12:56:01 -0700

    Tacos

---
+++ app.js
@@ -0,0 +1,139 @@
+const baseUrl = 'http://localhost:5000';
+const taquitosContainer = document.getElementById('taquitos-container');
+
+const btnPostTaco = document.getElementById('btn-post-taco');
+
+const tacosOptions = document.getElementById('taco-option');
+
+const tacoForm = {
+    name: document.getElementById('taco-name'),
+    quantity: document.getElementById('taco-quantity'),
+    pica: document.getElementById('option-spyciness')
+}
+
+btnPostTaco.onclick = ()=>{
+    const taco = {
+        name: tacoForm.name.value,
+        quantity: tacoForm.quantity.value,
+        pica: tacoForm.pica.value
+    }
+
+    AddTaquito(taco);
+};
+
+const GetTaquitos = ()=>{
+
+    //Otra forma de hacerlo
+    /*
+    const res = await fetch(baseUrl);
+    const data = await res.json();
+
+
+
+    
+    const tacosArray = data.map(element =>{
+        console.log(element);
+        const{id, name, quantity, pica} = element;
+        return{tacoId: id, tacoName: name, tacoQuantity: quantity, tacoPica: pica};
+    });
+
+    await tacosArray.forEach(element => {
+        //console.log(element);
+
+        tacosList.innerHTML += 
+        `<li>
+             ${element.tacoId}-Name: ${element.tacoName}
+             <ul>
+                    <div>-Location: ${element.tacoQuantity}</div>
+                    <div>-Dimension: ${element.tacoPica}</div>
+             </ul>
+        </li>`
+     });*/
+     
+
+    const url = baseUrl;
+    fetch(url)
+    .then(data => data.json())
+    .then(tacos => {
+        taquitosContainer.innerHTML = '';
+        tacosOptions.innerHTML = '';
+        tacos.forEach(taco => {
+            const tacoElement = document.createElement('div');
+            const tacoName = document.createElement('h3');
+            const tacoQuantity = document.createElement('div');
+            const tacoSpyciness = document.createElement('div');
+            const btnDeleteTaco = document.createElement('button');
+            btnDeleteTaco.innerHTML = 'Eliminar x';
+
+            const {name, quantity, pica, id} = taco;
+
+            btnDeleteTaco.onclick = ()=>{
+                DeleteTaquito(id);
+            }
+
+            const tacoOption = document.createElement('option');
+            tacoOption.value = id;
+            tacoOption.innerHTML = name;
+            tacosOptions.appendChild(tacoOption);
+
+            tacoQuantity.innerHTML = `cantidad: ${quantity}`;
+            tacoSpyciness.innerHTML = `¿Es picante?: ${pica}`;
+            tacoName.innerHTML = name;
+            tacoElement.appendChild(tacoName);
+            tacoElement.appendChild(tacoQuantity);
+            tacoElement.appendChild(tacoSpyciness);
+            tacoElement.appendChild(btnDeleteTaco);
+
+            taquitosContainer.appendChild(tacoElement);
+        });
+    });
+};
+
+const GetTaquito = id =>{
+    const url = `${baseUrl}/${id}`;
+    fetch(url)
+    .then(data => data.json())
+    .then(taco => {
+        
+    });
+};
+
+const AddTaquito = taco =>{
+    const url = baseUrl;
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(taco),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(data => data.json())
+    .then(taco => GetTaquitos());
+};
+
+const UpdateTaquito = (id, data) =>{
+    const url = `${baseUrl}/${id}`;
+    fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(taco => console.log(taco));
+};
+
+const DeleteTaquito = id =>{
+    const url = `${baseUrl}/${id}`;
+    fetch(url, {
+        method: 'DELETE'
+    })
+    .then(_=> {
+        GetTaquitos();
+    })
+};
+
+GetTaquitos();
+
+
+
